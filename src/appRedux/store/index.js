d1ec81f4784bb85ba386f0f64ed225a9dfdb79ef
Yogendra Singh <yogendra.singh@g-axon.com>
2019-06-18 11:36:00 +0530

    fixed build issue

--- src/appRedux/store/index.js
+++ src/appRedux/store/index.js
@@ -8,7 +8,7 @@ const history = createHistory();
 const routeMiddleware = routerMiddleware(history);
 
 const middlewares = [thunk, routeMiddleware];
-const composeEnhancers = process.env.NODE_ENV === "development" && (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore(initialState) {
   const store = createStore(reducers, initialState,
