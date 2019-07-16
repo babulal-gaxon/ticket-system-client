72705720bc437d6852f9d7c9a0a30859ce7c7256
Yogendra Singh <yogendra.singh@g-axon.com>
2019-07-16 12:20:25 +0530

    init customer panel

---
+++ src/routes/Sample/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+class Sample extends Component {
+
+  render() {
+    return (
+      <div className="gx-main-content">
+        hi sample
+      </div>
+    )
+  }
+}
+
+
+export default Sample;
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,13 +6,7 @@ import asyncComponent from "../util/asyncComponent";
 const App = ({match}) => (
     <div className="gx-main-content-wrapper">
         <Switch>
-            <Route path={`${match.url}dashboard`} component={asyncComponent(() => import('./Dashboard'))}/>
-            <Route path={`${match.url}manage-tickets`} component={asyncComponent(() => import('./ManageTickets'))}/>
-            <Route path={`${match.url}staff`} component={asyncComponent(() => import('./Staff'))}/>
-            <Route path={`${match.url}roles-permissions`}component={asyncComponent(() => import('./RolesAndPermissions'))}/>
-            <Route path={`${match.url}customers`} component={asyncComponent(() => import('./Customers'))}/>
-            <Route path={`${match.url}setup`} component={asyncComponent(() => import('./SetUp'))}/>
-            <Route path={`${match.url}settings`} component={asyncComponent(() => import('./Settings'))}/>
+            <Route path={`${match.url}settings`} component={asyncComponent(() => import('./Sample'))}/>
         </Switch>
     </div>
 );
