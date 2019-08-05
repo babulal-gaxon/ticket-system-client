7020b8010966f2a36de91fff11b0bc530ae62f98
Atul Midha <atul.midda@g-axon.com>
2019-08-05 18:30:18 +0530

    working on localization

--- src/appRedux/reducers/Common.js
+++ src/appRedux/reducers/Common.js
@@ -1,15 +1,26 @@
-import {FETCH_ERROR, FETCH_START, FETCH_SUCCESS, HIDE_MESSAGE, SHOW_MESSAGE} from '../../constants/ActionTypes'
+import {
+  FETCH_ERROR,
+  FETCH_START,
+  FETCH_SUCCESS,
+  HIDE_MESSAGE,
+  SHOW_MESSAGE,
+  UPDATING_CONTENT
+} from '../../constants/ActionTypes'
 
 const INIT_STATE = {
   error: "",
   loading: false,
-  message: ''
+  message: '',
+  updatingContent: false
 };
 
 export default (state = INIT_STATE, action) => {
   switch (action.type) {
     case FETCH_START: {
       return {...state, error: '', message: '', loading: true};
+    }
+    case UPDATING_CONTENT: {
+      return {...state, error: '', message: '', updatingContent: true};
     }
     case FETCH_SUCCESS: {
       return {...state, error: '', message: action.payload, loading: false};
--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -35,6 +35,7 @@ export const INIT_URL = 'INIT_URL';
 export const USER_DATA = 'user_data';
 export const UPDATE_USER_PERMISSION_DATA = 'update_user_permission_data';
 export const USER_TOKEN_SET = 'user_token_set';
+export const UPDATING_CONTENT = "UPDATING_CONTENT";
 
 
 
