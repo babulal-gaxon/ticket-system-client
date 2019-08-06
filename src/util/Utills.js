c98a087c84681afad63bd826e8cc84b67a72c39a
Atul Midha <atul.midda@g-axon.com>
2019-08-06 10:33:06 +0530

    working on date and time format

--- src/util/Utills.js
+++ src/util/Utills.js
@@ -1,9 +1,41 @@
+const moment = require('moment-timezone');
+const timeZone = moment.tz.guess();
+
+
 export const canEdit = () => {
   return true;
 };
+
 export const canAdd = () => {
   return true;
 };
+
 export const canDelete = () => {
   return false;
 };
+
+const convertToGMT = (date) => {
+  return date.replace(" ", "T").concat("Z")
+};
+
+export const getFormattedDate = (date) => {
+  return moment.tz(convertToGMT(date), timeZone).format(userSetting.locale.date_format.toUpperCase());
+};
+
+export const getFormattedTime = (time) => {
+  const is24Hr = userSetting.locale.time_format === "24 Hours";
+  if (is24Hr) {
+    return moment(convertToGMT(time)).tz(timeZone).format("HH:mm");
+  }
+  return moment(convertToGMT(time)).tz(timeZone).format("hh:mm A");
+};
+
+export const getFormattedDateTime = (timeStamp) => {
+  return getFormattedDate(timeStamp) + " " + getFormattedTime(timeStamp);
+};
+
+
+
+
+
+
