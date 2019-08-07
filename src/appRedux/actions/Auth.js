f24611e30f544687264a6933157bc9ea639fa8d5
Atul Midha <atul.midda@g-axon.com>
2019-08-07 10:42:11 +0530

    small change

--- src/appRedux/actions/Auth.js
+++ src/appRedux/actions/Auth.js
@@ -141,7 +141,6 @@ export const getUserProfile = () => {
 };
 
 export const updateUserProfile = (profile, context) => {
-  console.log("profile", profile);
   const {messages} = context.props.intl;
   return (dispatch) => {
     dispatch({type: FETCH_START});
